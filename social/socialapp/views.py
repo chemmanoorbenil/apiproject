bd00e0ed9de7bcf9c9813446edc8976124f4ac30
chemmanoorbenil <107665732+chemmanoorbenil@users.noreply.github.com>
2022-10-06 15:24:42 +0530

    Update views.py

--- social/socialapp/views.py
+++ social/socialapp/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from .models import *
+from .serializers import *
+from rest_framework.response import Response
 
 # Create your views here.
 
 
 class Register_user(APIView):
-    pass
+    def get(self, request):
+        student_obj = Profile.objects.all()
+        serializer = ProfileSerializer(student_obj, many=True)
+        return Response({'status': 200, 'payload': serializer.data})
